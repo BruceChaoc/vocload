af1192a59c6c864e0e05940bbfa4ec281ac2facf
chaoc <1197190972@qq.com>
2021-12-22 15:10:12 +0800

    base framework complete

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import os
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from lxml import etree
 
+fnt = ImageFont.truetype("simhei.ttf", 10)
 xmlLists = os.listdir("data_set/train/ANNOTATIONS")
+colorSelect = {'rolled-in_scale': 0xff0000,
+               'patches': 0x00ff00,
+               'crazing': 0x0000ff,
+               'pitted_surface': 0xee00ee,
+               'inclusion': 0x00eeee,
+               'scratches': 0xeeee00}
 
 for xmlList in xmlLists:
     # imName = xmlList.split(".", -1)[0]
@@ -16,15 +23,12 @@ for xmlList in xmlLists:
         with Image.open(os.path.join("data_set/train/IMAGES/", imFileName)) as im:
             drawR = ImageDraw.ImageDraw(im)
             for imXmlObj in imXmlObjs:
-                imObjName.append(imXmlObj.xpath("./name/text()"))
+                imObjName = imXmlObj.xpath("./name/text()")[0]
                 imObjBndbox=[int(imXmlObj.xpath(".//xmin/text()")[0]),
                              int(imXmlObj.xpath(".//ymin/text()")[0]),
                              int(imXmlObj.xpath(".//xmax/text()")[0]),
                              int(imXmlObj.xpath(".//ymax/text()")[0])]
-                drawR.rectangle((imObjBndbox[0], imObjBndbox[1]),
-                                (imObjBndbox[2], imObjBndbox[3]),
-                                fill=None, outline='red', width=1)
-                im.show()
-            im.save("drawR/train/IMAGES/0.jpg")
-            pass
+                drawR.rectangle(imObjBndbox, fill=None, outline=colorSelect[imObjName], width=1)
+                drawR.text((imObjBndbox[0], imObjBndbox[1]), imObjName, font=fnt, fill=colorSelect[imObjName])
+                im.save(os.path.join("drawR/train/IMAGES/", imFileName))
 
