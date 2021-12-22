f285be5b10a639541de7758ac412f03aa9624a60
chaoc <1197190972@qq.com>
2021-12-22 11:28:58 +0800

    base framework

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import os
+from PIL import Image, ImageDraw
+from lxml import etree
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+xmlLists = os.listdir("data_set/train/ANNOTATIONS")
 
+for xmlList in xmlLists:
+    # imName = xmlList.split(".", -1)[0]
+    with open(os.path.join("data_set/train/ANNOTATIONS/", xmlList)) as xml:
+        imObjName = []
+        imObjBndbox = []
+        imXml = etree.parse(xml)
+        # print(etree.tostring(imXml, pretty_print=True))
+        imFileName = imXml.xpath("./filename/text()")[0]
+        imXmlObjs = imXml.xpath("//object")
+        with Image.open(os.path.join("data_set/train/IMAGES/", imFileName)) as im:
+            drawR = ImageDraw.ImageDraw(im)
+            for imXmlObj in imXmlObjs:
+                imObjName.append(imXmlObj.xpath("./name/text()"))
+                imObjBndbox=[int(imXmlObj.xpath(".//xmin/text()")[0]),
+                             int(imXmlObj.xpath(".//ymin/text()")[0]),
+                             int(imXmlObj.xpath(".//xmax/text()")[0]),
+                             int(imXmlObj.xpath(".//ymax/text()")[0])]
+                drawR.rectangle((imObjBndbox[0], imObjBndbox[1]),
+                                (imObjBndbox[2], imObjBndbox[3]),
+                                fill=None, outline='red', width=1)
+                im.show()
+            im.save("drawR/train/IMAGES/0.jpg")
+            pass
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
